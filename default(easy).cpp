445e53116a683fec86301b38c213df5bb9f606f9
tori-az <sich208513@gmail.com>
2023-11-28 08:03:32 +0800

    11/28: add comments

---
+++ default(easy).cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define tori_az ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+/* cin/cout 較 scanf/printf 效率來得低，上面這行可以優化 cin/cout 的效率(原理可參照最後的網址) */
+#define endl '\n' /* '\n' 比 endl 快(原理可參照最後的網址)，由於筆者習慣使用 endl，便將 endl 定義為 '\n' */
+using namespace std;
+
+int main(){
+    tori_az
+
+    return 0;
+}
+
+// https://cp.wiwiho.me/io-optimize/ I/O 優化
