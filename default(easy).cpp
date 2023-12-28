14aa91c6f324a543332162aea67d0a75af8ed567
tori-az <sich208513@gmail.com>
2023-12-28 11:03:35 +0800

    12/28: add comment

--- default(easy).cpp
+++ default(easy).cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define tori_az ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-/* cin/cout 較 scanf/printf 效率來得低，上面這行可以優化 cin/cout 的效率，使用後程式碼內不可混用 scanf/printf */
+/* cin/cout 效率較 scanf/printf 低，上面這行可優化 cin/cout 的效率，使用後程式碼內不可混用 scanf/printf */
 #define endl '\n' /* '\n' 比 endl 快，由於筆者習慣使用 endl，便將 endl 定義為 '\n' */
 using namespace std;
 
