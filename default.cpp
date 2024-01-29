b35aa560f29837932306933a4c14233f81d56c2d
tori-az <sich208513@gmail.com>
2024-01-30 00:54:27 +0800

    01/30: add comments

--- default.cpp
+++ default.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
-#pragma GCC optimize("Ofast", "unroll-all-loops")
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/stack:200000000")
 #define tori_az ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define int long long 
 #define endl '\n'
